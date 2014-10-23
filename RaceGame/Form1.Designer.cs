db266a5d0edb627ae4242118d5a2bbfbca8f5ffc
dannynoppen <nopp1301@student.nhl.nl>
2014-10-23 09:05:03 +0200

    i did not do it

--- RaceGame/Form1.Designer.cs
+++ RaceGame/Form1.Designer.cs
@@ -43,10 +43,9 @@
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-<<<<<<< HEAD
-=======
+
             this.timer1 = new System.Windows.Forms.Timer(this.components);
->>>>>>> 72623df23caef941d3d3b968f5c3f5e7bfc6f4b5
+
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -154,33 +153,32 @@
             // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
-<<<<<<< HEAD
+
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
             this.label1.Location = new System.Drawing.Point(416, 598);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(0, 17);
             this.label1.TabIndex = 6;
-=======
+
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(821, 65);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(89, 46);
             this.label1.TabIndex = 6;
             this.label1.Text = "100";
->>>>>>> 72623df23caef941d3d3b968f5c3f5e7bfc6f4b5
+
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.BackColor = System.Drawing.Color.Transparent;
-<<<<<<< HEAD
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 23.25F);
             this.label4.Location = new System.Drawing.Point(848, 39);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(139, 35);
             this.label4.TabIndex = 7;
             this.label4.Text = "Ronde: 1";
-=======
+
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.Location = new System.Drawing.Point(686, 366);
             this.label4.Name = "label4";
@@ -193,22 +191,22 @@
             this.timer1.Enabled = true;
             this.timer1.Interval = 1000;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
->>>>>>> 72623df23caef941d3d3b968f5c3f5e7bfc6f4b5
+
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-<<<<<<< HEAD
+
             this.ClientSize = new System.Drawing.Size(1008, 747);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.fuelBar);
-=======
+
             this.ClientSize = new System.Drawing.Size(1344, 919);
             this.Controls.Add(this.label1);
->>>>>>> 72623df23caef941d3d3b968f5c3f5e7bfc6f4b5
+
             this.Controls.Add(this.fuelAmount);
             this.Controls.Add(this.fuelLabel);
             this.Controls.Add(this.label4);
@@ -244,10 +242,9 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label4;
-<<<<<<< HEAD
-=======
+
         public System.Windows.Forms.Timer timer1;
->>>>>>> 72623df23caef941d3d3b968f5c3f5e7bfc6f4b5
+
     }
 }
 
